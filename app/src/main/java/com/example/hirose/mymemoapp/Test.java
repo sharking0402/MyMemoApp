3d7805483af4b8815ae3a73fac37f75f926a62a0
arin-yaruta <arin.yaruta@gmail.com>
2015-06-04 14:52:25 +0900

    test

--- app/src/main/java/com/example/hirose/mymemoapp/Test.java
+++ app/src/main/java/com/example/hirose/mymemoapp/Test.java
@@ -4,5 +4,5 @@ package com.example.hirose.mymemoapp;
  * Created by hirose on 2015/06/04.
  */
 public class Test {
-    private String hoge = "hogehoge";
+    private String hoge = "hogehogehoge";
 }
